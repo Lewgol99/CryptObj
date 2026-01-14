a6970ec4393c54c4553aca28be0357b6ecd28688
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-01-14 13:21:24 +0000

    Update pki_setup.py

--- pki_setup.py
+++ pki_setup.py
@@ -2,8 +2,24 @@ from asymmetric_keys import Asymmetric_Keys
 from csr import CertificateSigningRequest
 
 class PKI:
-    def run_keys(self):
-        asymkeygen = Asymmetric_Keys()
+    def __init__(self):
+        self.keygen = None
+        self.self.private_key = None 
+
+    def generate_keys(self):
+        self.keygen = Asymmetric_Keys()
+        self.keygen.Generate_Private_key()
+        self.private_key = self.keygen.private_key
         
-    def run_csr(self):
+    def generate_csr(self):
+        if self.private_key is None:
+            print(Fore.RED + f'Error: Generate Keys First!')
+            return None
         csr = CertificateSigningRequest(private_key)
+        csr.Create_CSR()
+        csr.Save_CSR()
+
+pki = PKI()
+pki.generate_keys()
+pki.generate_csr()
+print(Fore.GREEN + 'Success: Send CSR to CA!')
