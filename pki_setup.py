326efc50ee8efff1a994c71af95e7b687695b6a2
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-01-14 13:09:52 +0000

    Create pki_setup.py

---
+++ pki_setup.py
@@ -0,0 +1,9 @@
+from asymmetric_keys import Asymmetric_Keys
+from csr import CertificateSigningRequest
+
+class PKI:
+    def run_keys(self):
+        asymkeygen = Asymmetric_Keys()
+        
+    def run_csr(self):
+        csr = CertificateSigningRequest(private_key)
