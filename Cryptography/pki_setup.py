67813ddfa2b1f1f39f2bde7257ab4b28b3de5cfa
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-01-14 16:03:37 +0000

    Create pki_setup.py

---
+++ Cryptography/pki_setup.py
@@ -0,0 +1,21 @@
+from asymmetric_keys import Asymmetric_Keys
+from csr import CertificateSigningRequest
+from colorama import Fore
+
+class PKI:
+    def __init__(self):
+        self.keygen = None
+        self.private_key = None 
+
+    def generate_keys(self):
+        self.keygen = Asymmetric_Keys()
+        self.keygen.Generate_Private_key()
+        self.private_key = self.keygen.private_key
+        
+    def generate_csr(self):
+        if self.private_key is None:
+            print(Fore.RED + f'Error: Generate Keys First!')
+            return None
+        csr = CertificateSigningRequest(self.private_key)
+        csr.Create_CSR()
+        csr.Save_CSR()
