9a4d4dbaf54c0b3d7c8e65efc8bf0e32b4323975
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-01-15 01:43:51 +0000

    Update request.py

--- CA/request.py
+++ CA/request.py
@@ -1,4 +1,16 @@
 import requests
+from colorama import Fore
 
-response = requests.get("http://192.168.66.252:5000/")
-print(response.json())
+CA_URL = "http://192.168.66.252:5000"
+
+def get_ca_status():
+    try:
+        response = requests.get(CA_URL)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(Fore.RED + f'Error: Failed to connect to CA server!')
+        return None
+
+status = get_ca_status()
+print(status)
