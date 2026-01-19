17956fec62c77582493835398e5a6974c1c2fb4d
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-01-19 16:09:06 +0000

    Update ca_server.py

--- CA/ca_server.py
+++ CA/ca_server.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from cryptography.hazmat.primitives import serialization
+from certificate_authority import CertificateAuthority
 
 app = Flask(__name__)
+
+ca = CertificateAuthority()
+ca.generate_ca_keys()
+ca.create_root_certificate()
 
 @app.route("/")
 def server_status():
@@ -8,7 +14,23 @@ def server_status():
         "service": 'CA Server',
         "status": 'Connected'
     }
+
+@app.route('/sign_csr', methods=['POST'])
+def sign_csr():
+    data = request.get_json()
+    csr_pem = data.get('csr')
+
+    with open('csr.pem', 'wb') as f:
+        f.write(csr_pem.encode())
+
+    csr = ca.load_csr()
+    if csr:
+        cert = ca.sign_csr(csr)
+        if cert:
+            cert_pem = cert.public_bytes(serialization.Encoding.PEM).decode()
+            return jsonify({'certificate': cert_pem}), 200
+    
+    return jsonify({'Error': 'Failed'}), 500
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-
