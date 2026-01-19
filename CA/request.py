b3ec03cacd4de22b3b202bb38c8ca09c61b5fd21
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-01-19 16:09:33 +0000

    Update request.py

--- CA/request.py
+++ CA/request.py
@@ -1,7 +1,24 @@
 import requests
 from colorama import Fore
 
-CA_URL = "http://192.168.66.252:5000"
+CA_URL = "http://192.168.66.253:5000"
+
+def submit_csr_to_ca():
+    try:
+        with open('csr.pem', 'r') as f:
+            csr_pem = f.read()
+        
+        response = requests.post(f'{CA_URL}/sign_csr', json={'csr': csr_pem})  
+        response.raise_for_status()
+
+        cert_pem = response.json()['certificate']
+        with open('certificate.pem', 'w') as f:
+            f.write(cert_pem)
+        print(Fore.GREEN + 'Success: Certificate received from CA!') 
+        return True
+    except Exception as e:
+        print(Fore.RED + f'Error: Failed to receive Certificate from CA! {e}') 
+        return False
 
 def get_ca_status():
     try:
@@ -9,7 +26,7 @@ def get_ca_status():
         response.raise_for_status()
         return response.json()
     except Exception as e:
-        print(Fore.RED + f'Error: Failed to connect to CA server!')
+        print(Fore.RED + f'Error: Failed to connect to CA server! {e}')
         return None
 
 status = get_ca_status()
