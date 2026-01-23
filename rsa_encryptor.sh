cb66e86c695a66ce2c925e3a101898fe062d5381
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-01-23 12:06:16 +0000

    Update rsa_encryptor.sh

--- rsa_encryptor.sh
+++ rsa_encryptor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp Original/encryptor.py $(python3 -c "import pysyncobj, os; print(os.path.dirname(pysyncobj.__file__))")/encryptor.py
+cp Source_Code/encryptor.py $(python3 -c "import pysyncobj, os; print(os.path.dirname(pysyncobj.__file__))")/encryptor.py
 echo "✓ RSA Encryptor Installed!"
