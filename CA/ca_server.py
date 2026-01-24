7918e4c0dfb27d47c52b39cf9c5d56942099d88e
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-01-24 12:05:32 +0000

    Update ca_server.py

--- CA/ca_server.py
+++ CA/ca_server.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 from cryptography.hazmat.primitives import serialization
 from certificate_authority import CertificateAuthority
+import os
 
 app = Flask(__name__)
+
+certs = 'issued certificates'
+os.makedirs(certs, exist_ok=True)
 
 ca = CertificateAuthority()
 ca.generate_ca_keys()
@@ -32,5 +36,19 @@ def sign_csr():
     
     return jsonify({'Error': 'Failed'}), 500
     
+@app.route('/api/get_all_certificates', methods=['GET'])
+def get_all_certificates():
+    """Return all issued certificates"""
+    certificates = {}
+    
+    # Read all .pem files from issued_certificates directory
+    for filename in os.listdir(CERTS_DIR):
+        if filename.endswith('_certificate.pem'):
+            node_name = filename.replace('_certificate.pem', '')
+            with open(f'{certs}/{filename}', 'r') as f:
+                certificates[node_name] = f.read()
+    
+    return jsonify({'certificates': certificates})
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
