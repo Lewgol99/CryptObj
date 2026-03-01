e2e72f1ac70fe2b638866b991bdda63c541b12e3
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-03-01 00:57:57 +0000

    Create src.sh

---
+++ src.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp transport.py $(python3 -c "import pysyncobj, os; print(os.path.dirname(pysyncobj.__file__))")/transport.py
+echo "✓ RSA Transport Installed!"
+
+cp encryptor.py $(python3 -c "import pysyncobj, os; print(os.path.dirname(pysyncobj.__file__))")/encryptor.py
+echo "✓ RSA Encryptor Installed!"
