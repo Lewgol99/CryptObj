1e2643ad75050bc9948f4ab64cc5401fc0650c18
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-03-02 12:51:33 +0000

    Update transport.py

--- Source Code/transport.py
+++ Source Code/transport.py
@@ -9,7 +9,7 @@ import os
 import threading
 import time
 import random
-
+from colorama import Fore
 
 class TransportNotReadyError(Exception):
     """Transport failed to get ready for operation."""
