b29acbba73f5440edd37730e1325808b0e86e611
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-03-10 16:09:43 +0000

    Update src.sh

--- src.sh
+++ src.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp transport.py $(python3 -c "import pysyncobj, os; print(os.path.dirname(pysyncobj.__file__))")/transport.py
-echo "✓ RSA Transport Installed!"
+echo "✓ RSA and ECC Transport Installed!"
 
 cp encryptor.py $(python3 -c "import pysyncobj, os; print(os.path.dirname(pysyncobj.__file__))")/encryptor.py
-echo "✓ RSA Encryptor Installed!"
+echo "✓ RSA and ECC Encryptor Installed!"
