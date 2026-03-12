06e4eb367aeb41c7cbc5fe0af531aef80ffd133f
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-03-12 13:27:57 +0000

    Delete src.sh

--- src.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp transport.py /usr/local/lib/python3.8/dist-packages/pysyncobj/transport.py
-echo "✓ Transport Installed!"
-cp encryptor.py /usr/local/lib/python3.8/dist-packages/pysyncobj/encryptor.py
-echo "✓ Encryptor Installed!"
