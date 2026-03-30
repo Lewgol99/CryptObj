ed421580bc57f1fd30e951d02f278f51be00da0e
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-03-30 12:47:54 +0100

    Update and rename botnet4.py to botnet.py

--- Security Evaluation/botnet.py
+++ Security Evaluation/botnet.py
@@ -57,6 +57,10 @@ as166.createNetwork('net9') # create net 9
 as166.createNetwork('net10') # create net 10
 as166.createHost('host0').joinNetwork('net0')
 router166 = as166.createRealWorldRouter('branch', prefixes=['0.0.0.0/1', '128.0.0.0/1'])
+router.addSoftware('git')
+router.addSoftware('python3')
+router.addBuildCommand(f'git clone https://{GIT_USERNAME}:{GIT_TOKEN}@github.com/Lewgol99/PySyncCryptObj.git')
+router.addBuildCommand(f'chmod -R 777 PySyncCryptObj')
 router166.joinNetwork('net0') # join net 0
 router166.joinNetwork('net1') # join net 1
 router166.joinNetwork('net2') # join net 2
@@ -86,6 +90,10 @@ as176.createNetwork('net8') # create net 8
 as176.createNetwork('net9') # create net 9
 as176.createHost('host0').joinNetwork('net0')
 router176 = as176.createRealWorldRouter('branch', prefixes=['0.0.0.0/1', '128.0.0.0/1'])
+router.addSoftware('git')
+router.addSoftware('python3')
+router.addBuildCommand(f'git clone https://{GIT_USERNAME}:{GIT_TOKEN}@github.com/Lewgol99/PySyncCryptObj.git')
+router.addBuildCommand(f'chmod -R 777 PySyncCryptObj')
 router176.joinNetwork('net0') # join net 0
 router176.joinNetwork('net1') # join net 1
 router176.joinNetwork('net2') # join net 2
@@ -114,6 +122,10 @@ as186.createNetwork('net8') # create net 8
 as186.createNetwork('net9') # create net 9
 as186.createHost('host0').joinNetwork('net0')
 router186 = as186.createRealWorldRouter('branch', prefixes=['0.0.0.0/1', '128.0.0.0/1'])
+router.addSoftware('git')
+router.addSoftware('python3')
+router.addBuildCommand(f'git clone https://{GIT_USERNAME}:{GIT_TOKEN}@github.com/Lewgol99/PySyncCryptObj.git')
+router.addBuildCommand(f'chmod -R 777 PySyncCryptObj')
 router186.joinNetwork('net0') # join net 0
 router186.joinNetwork('net1') # join net 1
 router186.joinNetwork('net2') # join net 2
@@ -142,6 +154,10 @@ as196.createNetwork('net8') # create net 8
 as196.createNetwork('net9') # create net 9
 as196.createHost('host0').joinNetwork('net0')
 router196 = as196.createRealWorldRouter('branch', prefixes=['0.0.0.0/1', '128.0.0.0/1'])
+router.addSoftware('git')
+router.addSoftware('python3')
+router.addBuildCommand(f'git clone https://{GIT_USERNAME}:{GIT_TOKEN}@github.com/Lewgol99/PySyncCryptObj.git')
+router.addBuildCommand(f'chmod -R 777 PySyncCryptObj')
 router196.joinNetwork('net0') # join net 0
 router196.joinNetwork('net1') # join net 1
 router196.joinNetwork('net2') # join net 2
@@ -177,7 +193,7 @@ else:
     print("Warning: ddos.py not found")
 
 # Create bot nodes
-num_bots = 4
+num_bots = 250
 for counter in range(num_bots):
     vname = f'bot-node-{counter:03d}'
     botClient.install(vname).setServer('bot-controller')
@@ -216,4 +232,3 @@ emu.addLayer(botClient)
 emu.dump('base_tx-pbac.bin')
 emu.render()
 emu.compile(Docker(), './output', override=True)
-
