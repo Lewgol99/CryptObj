0d1b4eff92687e036b0be615ffc25a958c6bc2ef
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-05-01 00:57:48 +0100

    Update peering.py

--- Emulator/peering.py
+++ Emulator/peering.py
@@ -1,15 +1,3 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-
-# Import the SEED Library
-from seedemu import *
-from seedemu.layers import Base, Routing, Ebgp, PeerRelationship, Ibgp, Ospf
-from seedemu.services import WebService
-from seedemu.core import Emulator
-from seedemu.compiler import Docker
-
-###############################################################################
-
 # Emulator Setup
 emu = Emulator()
 base = Base()
@@ -41,6 +29,10 @@ router166 = as166.createRealWorldRouter('branch', prefixes=['0.0.0.0/1', '128.0.
 router166.joinNetwork('ix100', '10.100.0.166')
 router166.addSoftware('git')
 router166.addSoftware('python3')
+router166.addBuildCommand(f'git clone https://{GIT_USERNAME}:{GIT_TOKEN}@github.com/Lewgol99/CryptObj.git')
+router166.addBuildCommand(f'chmod -R 777 CryptObj')
+router166.addBuildCommand('apt-get install -y --no-install-recommends lftp python3-pip && apt-get clean && rm -rf /var/lib/apt/lists/*')
+router166.addBuildCommand('pip3 install --no-cache-dir -r CryptObj/requirements.txt')
 
 # AS176
 as176 = base.createAutonomousSystem(176)
@@ -48,6 +40,10 @@ router176 = as176.createRealWorldRouter('branch', prefixes=['0.0.0.0/1', '128.0.
 router176.joinNetwork('ix100', '10.100.0.176')
 router176.addSoftware('git')
 router176.addSoftware('python3')
+router176.addBuildCommand(f'git clone https://{GIT_USERNAME}:{GIT_TOKEN}@github.com/Lewgol99/CryptObj.git')
+router176.addBuildCommand(f'chmod -R 777 CryptObj')
+router176.addBuildCommand('apt-get install -y --no-install-recommends lftp python3-pip && apt-get clean && rm -rf /var/lib/apt/lists/*')
+router176.addBuildCommand('pip3 install --no-cache-dir -r CryptObj/requirements.txt')
 
 # AS186
 as186 = base.createAutonomousSystem(186)
@@ -55,6 +51,10 @@ router186 = as186.createRealWorldRouter('branch', prefixes=['0.0.0.0/1', '128.0.
 router186.joinNetwork('ix100', '10.100.0.186')
 router186.addSoftware('git')
 router186.addSoftware('python3')
+router186.addBuildCommand(f'git clone https://{GIT_USERNAME}:{GIT_TOKEN}@github.com/Lewgol99/CryptObj.git')
+router186.addBuildCommand(f'chmod -R 777 CryptObj')
+router186.addBuildCommand('apt-get install -y --no-install-recommends lftp python3-pip && apt-get clean && rm -rf /var/lib/apt/lists/*')
+router186.addBuildCommand('pip3 install --no-cache-dir -r CryptObj/requirements.txt')
 
 # AS196
 as196 = base.createAutonomousSystem(196)
@@ -62,6 +62,10 @@ router196 = as196.createRealWorldRouter('branch', prefixes=['0.0.0.0/1', '128.0.
 router196.joinNetwork('ix100', '10.100.0.196')
 router196.addSoftware('git')
 router196.addSoftware('python3')
+router196.addBuildCommand(f'git clone https://{GIT_USERNAME}:{GIT_TOKEN}@github.com/Lewgol99/CryptObj.git')
+router196.addBuildCommand(f'chmod -R 777 CryptObj')
+router196.addBuildCommand('apt-get install -y --no-install-recommends lftp python3-pip && apt-get clean && rm -rf /var/lib/apt/lists/*')
+router196.addBuildCommand('pip3 install --no-cache-dir -r CryptObj/requirements.txt')
 ###############################################################################
 
 # Peering
