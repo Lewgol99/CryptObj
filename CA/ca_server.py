64613b715f04b57757ff70231e9e0b9fe2e550c9
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-05-30 00:01:04 +0100

    Update ca_server.py

--- CA/ca_server.py
+++ CA/ca_server.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify
 from cryptography.hazmat.primitives import serialization
 from certificate_authority import CertificateAuthority
+import threading
 import os
 
 app = Flask(__name__)
-
+lock = threading.Lock()
 certs = 'issued certificates'
 os.makedirs(certs, exist_ok=True)
-
 ca = CertificateAuthority()
 ca.generate_ca_keys()
 ca.create_root_certificate()
@@ -23,27 +23,21 @@ def server_status():
 def sign_csr():
     data = request.get_json()
     csr_pem = data.get('csr')
-    node_name = data.get('node_name')  # Add this
-
-    with open('csr.pem', 'wb') as f:
-        f.write(csr_pem.encode())
-
-    csr = ca.load_csr()
-    if csr:
-        cert = ca.sign_csr(csr)
-        if cert:
-            cert_pem = cert.public_bytes(serialization.Encoding.PEM).decode()
-            
-            # Store it automatically
-            os.makedirs('issued_certificates', exist_ok=True)
-            with open(f'issued_certificates/{node_name}_certificate.pem', 'w') as f:
-                f.write(cert_pem)
-            print(f'✓ Stored {node_name}_certificate.pem')
-            
-            return jsonify({'certificate': cert_pem}), 200
-    
+    node_name = data.get('node_name')
+    with lock:
+        with open('csr.pem', 'wb') as f:
+            f.write(csr_pem.encode())
+        csr = ca.load_csr()
+        if csr:
+            cert = ca.sign_csr(csr)
+            if cert:
+                cert_pem = cert.public_bytes(serialization.Encoding.PEM).decode()
+                os.makedirs('issued_certificates', exist_ok=True)
+                with open(f'issued_certificates/{node_name}_certificate.pem', 'w') as f:
+                    f.write(cert_pem)
+                print(f'✓ Stored {node_name}_certificate.pem')
+                return jsonify({'certificate': cert_pem}), 200
     return jsonify({'Error': 'Failed'}), 500
 
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
