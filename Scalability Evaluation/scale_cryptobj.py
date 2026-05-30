f256505a89e987fdbbd3b34a6a6a313bca3d4bbf
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-05-30 13:32:18 +0100

    Update scale_cryptobj.py

--- Scalability Evaluation/scale_cryptobj.py
+++ Scalability Evaluation/scale_cryptobj.py
@@ -1,7 +1,6 @@
 import sys
 import time 
 import json
-import threading
 from colorama import Fore, Style
 from functools import partial
 from pysyncobj import SyncObj, replicated, SyncObjConf
@@ -169,8 +168,7 @@ if __name__ == '__main__':
             pki.generate_ecc_keys(curve_name)
             pki.generate_csr()
             result = submit_csr_to_ca(node_name)
-        print(Fore.CYAN + 'Fetching all certificates in background...')
-        threading.Thread(target=fetch_all_certificates, args=(node_name,), daemon=True).start()
+        fetch_all_certificates(node_name)
 
     if not os.path.exists('signing_private_key.pem'):
         print(Fore.YELLOW + 'No signing key found, generating...')
