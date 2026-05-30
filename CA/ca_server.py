004a6926d935d0f0da68e3adf308f5f1e3543ba3
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-05-30 01:07:35 +0100

    Update ca_server.py

--- CA/ca_server.py
+++ CA/ca_server.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from cryptography.hazmat.primitives import serialization
+from cryptography.x509 import load_pem_x509_csr
 from certificate_authority import CertificateAuthority
 import threading
 import os
 
 app = Flask(__name__)
 lock = threading.Lock()
-certs = 'issued certificates'
+certs = 'issued_certificates'
 os.makedirs(certs, exist_ok=True)
 ca = CertificateAuthority()
 ca.generate_ca_keys()
@@ -24,19 +25,18 @@ def sign_csr():
     data = request.get_json()
     csr_pem = data.get('csr')
     node_name = data.get('node_name')
-    with lock:
-        with open('csr.pem', 'wb') as f:
-            f.write(csr_pem.encode())
-        csr = ca.load_csr()
-        if csr:
+    try:
+        csr = load_pem_x509_csr(csr_pem.encode())
+        with lock:
             cert = ca.sign_csr(csr)
-            if cert:
-                cert_pem = cert.public_bytes(serialization.Encoding.PEM).decode()
-                os.makedirs('issued_certificates', exist_ok=True)
-                with open(f'issued_certificates/{node_name}_certificate.pem', 'w') as f:
-                    f.write(cert_pem)
-                print(f'✓ Stored {node_name}_certificate.pem')
-                return jsonify({'certificate': cert_pem}), 200
+        if cert:
+            cert_pem = cert.public_bytes(serialization.Encoding.PEM).decode()
+            with open(f'issued_certificates/{node_name}_certificate.pem', 'w') as f:
+                f.write(cert_pem)
+            print(f'✓ Stored {node_name}_certificate.pem')
+            return jsonify({'certificate': cert_pem}), 200
+    except Exception as e:
+        print(f'Error: {e}')
     return jsonify({'Error': 'Failed'}), 500
 
 if __name__ == '__main__':
