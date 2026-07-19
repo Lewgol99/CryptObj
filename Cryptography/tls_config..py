f74dc6a54a1bb228e2167c4d495bb3291b35ba73
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-07-19 19:10:02 +0100

    Update tls_config..py

--- Cryptography/tls_config..py
+++ Cryptography/tls_config..py
@@ -1,4 +1,5 @@
 import ssl
+import socket
 import json
 from colorama import Fore
 
@@ -7,32 +8,63 @@ class TLS:
     self.ciphers = ciphers
     self.group = group
 
+
   def open_cipher_suite(self):
       try:
           with open("tls_ciphers.json", "r") as file:
               tls-ciphers = json.load(file)
       except FileNotFoundError:
-          print("Error: 'tls_ciphers.json' file was not found.")
+          print(Fore.RED + "Error: 'tls_ciphers.json' file was not found.")
+          return None
           
   def tls_server(self):
-      try:
-         
-          
-           
-  def tls_client(self):
-     try:
-        context.load_verify_locations(cafile=cert_file)
+    try:
+        self.server_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        self.server_context.load_cert_chain(certfile="mycertfile", keyfile="mykeyfile")
+        SELF.bindsocket = socket.socket()
+        print(Fore.GREEN + 'Success: TLS Server Created!')
+        return True
     except Exception as e:
-  print('')
-     
-  def tls_connect(self):
-     return
-  
-  def tls_handshake(self):
-     return
-  
-  def tls_channel(self):
-     return
-     
- 
+        print(Fore.RED + 'Error: TLS Server Failed!')
+        return None
+    
+                    
+  def tls_client(self):
+    try:
+        self.client_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        self.client_context.minimum_version = ssl.TLSVersion.TLSv1_3
+        self.client_context.maximum_version = ssl.TLSVersion.TLSv1_3
+        self.client_context.load_verify_locations(cafile='certificate.pem')
+        print(Fore.GREEN + 'Success: TLS Client Node Created')
+        return True
+    except Exception as e:
+        print(Fore.RED + 'Error: TLS Client Node Failed!')
+        return None
 
+
+  def tls_connect(self):
+    try:
+        print(Fore.GREEN + 'Success:')
+        return True
+    except Exception as e:
+        print(Fore.RED + 'Error:')
+        return None
+  
+
+  def tls_handshake(self):
+    try:
+        print(Fore.GREEN + 'Success:')
+        return True
+    except Exception as e:
+        print(Fore.RED + 'Error:')
+        return None
+  
+
+  def tls_channel(self):
+    try:
+        print(Fore.GREEN + 'Success:')
+        return True
+    except Exception as e:
+        print(Fore.RED + 'Error:')
+        return None
+     
