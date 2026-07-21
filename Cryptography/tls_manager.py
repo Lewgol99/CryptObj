c23515d83bb46e0925f874c42665a0b517519eab
Lewgol99 <48607213+Lewgol99@users.noreply.github.com>
2026-07-21 12:37:37 +0100

    Update tls_manager.py

--- Cryptography/tls_manager.py
+++ Cryptography/tls_manager.py
@@ -1,15 +1,26 @@
+import json
+import os
 from tls_session import TLS_Session
+from latency_monitor import LatencyMonitor
  
 CA_CERT_FILE = 'certificate.pem'
 SELF_KEY_FILE = 'pki_private_key.pem'
- 
+
 class TLS_Manager:
-    def __init__(self, self_node_name, self_address, peer_addresses):
-        self.self_node_name = self_node_name
-        self.self_address = self_address
-        self.peer_addresses = peer_addresses
-        self._sessions = {}
+    def __init__(self):
+        nodes_file = 'scale_nodes.json' if os.path.exists('scale_nodes.json') else 'nodes.json'
+        with open(nodes_file, 'r') as f:
+            nodes = json.load(f)
  
+        self.self_node_name = os.environ['NODE_NAME']
+        self.self_address = f"{nodes[self.self_node_name]['addr']}:{nodes[self.self_node_name]['port']}"
+        self.peer_addresses = {
+            name: f"{info['addr']}:{info['port']}"
+            for name, info in nodes.items() if name != self.self_node_name
+        }
+        self.latency_monitor = LatencyMonitor()
+        self._sessions = {}
+     
     def _is_client_for(self, peer_node_name):
         return self.self_address > self.peer_addresses[peer_node_name]
  
@@ -22,5 +33,7 @@ class TLS_Manager:
                 self_cert_file=f'{self.self_node_name}_certificate.pem',
                 self_key_file=SELF_KEY_FILE,
                 ca_cert_file=CA_CERT_FILE,
+                latency_monitor=self.latency_monitor,
             )
         return self._sessions[peer_node_name]
+ 
